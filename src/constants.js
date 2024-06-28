43721f9e6b9d0e79eeb7b0afd9196eced490b6a1
Adan Attique <adanattique587@gmail.com>
2024-06-28 15:36:00 +0500

    all setup configured

--- src/constants.js
+++ src/constants.js
@@ -1,2 +1,3 @@
-export const DB_NAME = 'videotube'; 
+export const DB_NAME = "mydatabase"; // Replace 'mydatabase' with your actual database name
+
 
--- src/db/index.js
+++ src/db/index.js
@@ -1,12 +1,18 @@
 import mongoose from "mongoose";
+import dotenv from "dotenv";
 import { DB_NAME } from "../constants.js";
+
+dotenv.config();
 
 const connectDB = async () => {
     try {
-        const connectionInstance = await mongoose.connect(`${process.env.MONGODB_URL}/${DB_NAME}`);
+        const connectionInstance = await mongoose.connect(`${process.env.MONGODB_URL}/${DB_NAME}`, {
+            useNewUrlParser: true,
+            useUnifiedTopology: true,
+        });
         console.log(`\n MongoDB connected !!! DB HOST : ${connectionInstance.connection.host}`);
     } catch (error) {
-        console.log("MONGODB connection error", error);
+        console.error("MONGODB connection error", error);
         process.exit(1);
     }
 };
